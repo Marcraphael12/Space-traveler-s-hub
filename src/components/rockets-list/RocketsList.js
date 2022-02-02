9e47ae3e62d6cdf6388a64a38bc22d0b4dcd2a0d
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-02-02 17:14:28 +0600

    Fix my profile styles

--- src/components/rockets-list/RocketsList.js
+++ src/components/rockets-list/RocketsList.js
@@ -1,23 +1,20 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchRocketsAsync } from '../../redux/rockets/thunk';
+import { useSelector } from 'react-redux';
 import './rockets-list.scss';
 
 const RocketsList = () => {
   const rockets = useSelector((state) =>
     state.rocketsReducer.rockets.filter((rocket) => rocket.reserved)
   );
-  const dispatch = useDispatch();
-
-  useEffect(
-    () => !rockets.length && dispatch(fetchRocketsAsync()),
-    [dispatch, rockets.length]
-  );
 
   return (
     <div className="rockets-list">
       <h2>My rockets</h2>
       <ul className="rockets-list__rockets">
+        {!rockets.length && (
+          <li className="rockets-list__rocket text-disabled">
+            No rockets reserved...
+          </li>
+        )}
         {rockets.map((rocket) => (
           <li className="rockets-list__rocket">{rocket.rocket_name}</li>
         ))}
