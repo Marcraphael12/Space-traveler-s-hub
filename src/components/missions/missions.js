9ce7463f90071630f41790681a94456e5c2f8c2b
Marc raphael <marcraphael979@gmail.com>
2022-02-02 16:17:41 +0100

    components/missions/missions: Fix errors

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { missionFromApi } from '../../redux/missions/missions';
